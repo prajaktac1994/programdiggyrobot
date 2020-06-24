bc593e2e46e2a8d659cfa3ebf49d45138818cde8
prajaktac1994 <67221661+prajaktac1994@users.noreply.github.com>
2020-06-24 07:47:50 -0700

    Update DiggyRobot.java

--- DiggyRobot.java
+++ DiggyRobot.java
@@ -1,9 +1,10 @@
 public class DiggyRobot{
 	public static void main(String args[]){
-		int x=0,y=0,NORTH = 1,EAST = 2,SOUTH = 3,WEST = 4,add_1 = 1,add_2 = 1,add_3 = -1,add_4 = -1,directionNum = 0;
+		final int NORTH = 1,EAST = 2,SOUTH = 3,WEST = 4;
+		int x=0,y=0,directionNum = 0;
 		int multiplierVal[] = {0,1,1,-1,-1};
 		String direction[] = {" ","NORTH","EAST", "SOUTH","WEST"};
-		String presDir,path,pathInput;
+		String presDir,pathInput;
 		pathInput = args[3];
 		try{
     		x = Integer.parseInt(args[0]);
@@ -12,9 +13,20 @@ public class DiggyRobot{
     			System.out.println("Please Enter Valid Co-ordinates");
 		}
 		presDir = args[2];
+		if(presDir.equals("NORTH")){
+		  directionNum = 1;  
+		}else if(presDir.equals("EAST")){
+		     directionNum = 2;
+		}else if(presDir.equals("SOUTH")){
+		     directionNum = 3;
+		}else if(presDir.equals("WEST")){
+		     directionNum = 4;
+		}
 		
-		if(presDir != "NORTH" || presDir != "EAST" || presDir != "SOUTH" || presDir != "WEST"){
-			System.out.println("Please Enter Valid Direction");
+		if(presDir.equals("NORTH") || presDir.equals("EAST") || presDir.equals("SOUTH") || presDir.equals("WEST")){
+		
+		}else{
+		    	System.out.println("Please Enter Valid Direction");
 		}
 		char[] ch = new char[pathInput.length()]; 
   
@@ -24,14 +36,14 @@ public class DiggyRobot{
 		for(int i = 0; i < ch.length; i++ ){
 			switch(ch[i]){
 						case 'R':
-							if(presDir == "WEST"){
+							if(presDir.equals("WEST")){
 								directionNum = WEST;
 							}else {
 								directionNum++;
 							}
 							break;
                 		case 'L':
-            				if(presDir == "NORTH"){
+            				if(presDir.equals("NORTH")){
                     				directionNum = NORTH;
             				} else {
                     				directionNum--;
@@ -40,9 +52,9 @@ public class DiggyRobot{
                 		case 'W':
                 		    int multiplier = multiplierVal[directionNum];
 							if(directionNum % 2 == 1){
-							    y += (ch[i+1] * multiplier);
+							    y = (ch[i+1] * multiplier);
 							} else {
-							    x += (ch[i+1] * multiplier);
+							    x = (ch[i+1] * multiplier);
 								
 							}
 							i++;
@@ -53,16 +65,6 @@ public class DiggyRobot{
 
 			}
 		}
-		int position=0;
-		if(presDir == "NORTH"){
-		  position = 1;  
-		}else if(presDir == "EAST"){
-		     position = 2;
-		}else if(presDir == "SOUTH"){
-		     position = 3;
-		}else if(presDir == "WEST"){
-		     position = 4;
-		}
-		System.out.println(x+" "+y+" "+direction[position]+"\n");
+		System.out.println(x+" "+y+" "+direction[directionNum]+"\n");
 	}
 }
